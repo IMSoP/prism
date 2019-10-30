81c9db23f8926e2ee4fd5ad0f8963b8a449af4e4
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-30 18:46:49 -0500

    add input validation support

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import { json, text, send } from 'micri';
 import micri from 'micri';
 import route from '@stoplight/prism-http/dist/router';
 import mock from '@stoplight/prism-http/dist/mocker';
+import { validateInput } from '@stoplight/prism-http/dist/validator'
 import { createLogger } from '@stoplight/prism-core/dist/logger';
 import * as typeis from 'type-is';
-import { HttpMethod } from '@stoplight/types';
+import { HttpMethod, IHttpOperation } from '@stoplight/types';
 import grabOperationsSomehow from './grabOperations'
 import { pipe } from 'fp-ts/lib/pipeable'
 import * as R from 'fp-ts/lib/Reader'
+import * as E from 'fp-ts/lib/Either'
 import * as RE from 'fp-ts/lib/ReaderEither'
-import { ProblemJsonError } from '@stoplight/prism-core';
+import { ProblemJsonError, IPrismDiagnostic } from '@stoplight/prism-core';
+import { IHttpRequest } from '@stoplight/prism-http';
 
 function createPrismInput(url: string, body: unknown, method: string) {
   const parsedUrl = new URL(url, 'http://localhost:3000');
@@ -26,18 +29,27 @@ function createPrismInput(url: string, body: unknown, method: string) {
 
 const logger = createLogger('Server');
 
+function validateInputAndMock(resource: IHttpOperation, element: IHttpRequest) {
+  return pipe(
+    validateInput({ resource, element }),
+    E.fold<IPrismDiagnostic[], unknown, IPrismDiagnostic[]>(validations => validations, () => []),
+    validations => mock({ resource, input: { data: element, validations }, config: { dynamic: true } })
+  )
+}
+
 const server = micri(async function requestHandler(req, res) {
+  const resourcesPromise = grabOperationsSomehow();
   const body = await (typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req));
   const input = createPrismInput(req.url!, body, req.method!);
-  const resources = await grabOperationsSomehow();
+
+  const resources = await resourcesPromise;
 
   return pipe(
     RE.fromEither(route({ resources, input })),
-    RE.chain(resource => mock({ resource, input: { data: input, validations: [] }, config: { dynamic: true } })),
+    RE.chain(resource => validateInputAndMock(resource, input)),
     RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
     RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
   )(logger);
-
 });
 
 server.listen(process.env.PORT || 3000, () => console.info('Ready'));
