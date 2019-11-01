8e61b122dd9c439490ff6d53683b6f82fbf7805c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-01 12:00:19 -0500

    protect against undefined paths

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,11 @@ import { pipe } from 'fp-ts/lib/pipeable'
 import * as R from 'fp-ts/lib/Reader'
 import * as E from 'fp-ts/lib/Either'
 import * as RE from 'fp-ts/lib/ReaderEither'
+import * as O from 'fp-ts/lib/Option';
 import { ProblemJsonError, IPrismDiagnostic } from '@stoplight/prism-core';
 import { IHttpRequest, IHttpOperationConfig } from '@stoplight/prism-http';
 // @ts-ignore
 import { URI } from 'uri-template-lite';
-
 
 const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
@@ -49,22 +49,26 @@ function readConfigFromQueryString(queryString: URLSearchParams): IHttpOperation
 }
 
 const server = micri(async function requestHandler(req, res) {
-  const params: { project: string, prismUrl: string[] } = new URI.Template('/{project}{/prismUrl*}').match(req.url);
-  const resourcesPromise = grabOperationsSomehow(params.project);
-  const bodyPromise = (typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req));
-  const parsedUrl = new URL(params.prismUrl.join('/'), baseUrl);
-  const configFromQueryString = readConfigFromQueryString(parsedUrl.searchParams);
+  pipe(
+    O.fromNullable<{ project: string, prismUrl: string[] }>(new URI.Template('/{project}{/prismUrl*}').match(req.url)),
+    O.fold(() => send(res, 404), async params => {
+      const resourcesPromise = grabOperationsSomehow(params.project);
+      const bodyPromise = (typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req));
+      const parsedUrl = new URL(params.prismUrl.join('/'), baseUrl);
+      const configFromQueryString = readConfigFromQueryString(parsedUrl.searchParams);
 
-  const body = await bodyPromise;
-  const input = createPrismInput(parsedUrl, body, req.method!);
-  const resources = await resourcesPromise;
+      const body = await bodyPromise;
+      const input = createPrismInput(parsedUrl, body, req.method!);
+      const resources = await resourcesPromise;
 
-  return pipe(
-    RE.fromEither(route({ resources, input })),
-    RE.chain(resource => validateInputAndMock(resource, input, configFromQueryString)),
-    RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
-    RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
-  )(logger);
+      return pipe(
+        RE.fromEither(route({ resources, input })),
+        RE.chain(resource => validateInputAndMock(resource, input, configFromQueryString)),
+        RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
+        RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
+      )(logger);
+    })
+  );
 });
 
 server.listen(process.env.PORT || 3000, () => console.info('Ready'));
