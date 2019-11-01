cb8d7b111484ebefc51b4a5f2872fd91e621a564
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-01 09:29:03 -0500

    read config from query string

--- src/index.ts
+++ src/index.ts
@@ -12,43 +12,51 @@ import * as R from 'fp-ts/lib/Reader'
 import * as E from 'fp-ts/lib/Either'
 import * as RE from 'fp-ts/lib/ReaderEither'
 import { ProblemJsonError, IPrismDiagnostic } from '@stoplight/prism-core';
-import { IHttpRequest } from '@stoplight/prism-http';
+import { IHttpRequest, IHttpOperationConfig } from '@stoplight/prism-http';
 
 const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
-function createPrismInput(url: string, body: unknown, method: string) {
-  const parsedUrl = new URL(url, baseUrl);
-
+function createPrismInput(url: URL, body: unknown, method: string) {
   return {
     body,
     method: method as HttpMethod,
     url: {
-      path: parsedUrl.pathname,
-      query: Object.fromEntries(parsedUrl.searchParams.entries())
+      path: url.pathname,
+      query: Object.fromEntries(url.searchParams.entries())
     }
   };
 }
 
 const logger = createLogger('Server');
 
-function validateInputAndMock(resource: IHttpOperation, element: IHttpRequest) {
+function validateInputAndMock(resource: IHttpOperation, element: IHttpRequest, config: IHttpOperationConfig) {
   return pipe(
     validateInput({ resource, element }),
     E.fold<IPrismDiagnostic[], unknown, IPrismDiagnostic[]>(validations => validations, () => []),
-    validations => mock({ resource, input: { data: element, validations }, config: { dynamic: true } })
+    validations => mock({ resource, input: { data: element, validations }, config })
   )
+}
+
+function readConfigFromQueryString(queryString: URLSearchParams): IHttpOperationConfig {
+  return {
+    code: queryString.get('__code') || undefined,
+    exampleKey: queryString.get('__example') || undefined,
+    dynamic: queryString.get('__dynamic') === 'true'
+  }
 }
 
 const server = micri(async function requestHandler(req, res) {
   const resourcesPromise = grabOperationsSomehow();
   const body = await (typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req));
-  const input = createPrismInput(req.url!, body, req.method!);
+  const parsedUrl = new URL(req.url!, baseUrl);
+  const input = createPrismInput(parsedUrl, body, req.method!);
+  const configFromQueryString = readConfigFromQueryString(parsedUrl.searchParams)
 
   const resources = await resourcesPromise;
 
   return pipe(
     RE.fromEither(route({ resources, input })),
-    RE.chain(resource => validateInputAndMock(resource, input)),
+    RE.chain(resource => validateInputAndMock(resource, input, configFromQueryString)),
     RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
     RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
   )(logger);
