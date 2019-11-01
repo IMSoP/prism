5c416ac032b436ce790736dfa22ac340cec6208b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-01 13:52:13 -0500

    support multiple apis

--- src/grabOperations.ts
+++ src/grabOperations.ts
@@ -3,26 +3,29 @@ import { IHttpOperation } from '@stoplight/types';
 //@ts-ignore
 import * as fetchFactory from 'make-fetch-happen';
 
-const fetch: typeof import('node-fetch').default = fetchFactory.defaults({ cacheManager: './cache' });
+const fetch: typeof import('node-fetch').default = fetchFactory.defaults({
+  cacheManager: './cache',
+});
 
-export default async function grabOperationsSomehow(project: string): Promise<IHttpOperation[]> {
+export default async function grabOperationsSomehow(project: string, serviceName: string): Promise<IHttpOperation[]> {
   type node = {
     type: string;
     id: string;
     srn: string;
   };
   const projectNodes: { items: node[] } = await fetch(
-    `https://stoplight.io/api/projects.nodes?srn=gh/stoplightio/${project}`
+    `https://stoplight.io/api/projects.nodes?srn=gh/stoplightio/${encodeURIComponent(project)}`
   ).then(d => d.json());
+
+  const service = projectNodes.items.find(t => t.type === 'http_service' && t.srn.includes(serviceName));
 
   return Promise.all(
     projectNodes.items
-      .filter(t => t.type === 'http_operation')
+      .filter(t => t.type === 'http_operation' && t.srn.indexOf(service!.srn))
       .map(f =>
-        fetch(`https://stoplight.io/api/nodes.raw?srn=${f.srn}&id=${f.id}`)
+        fetch(`https://stoplight.io/api/nodes.raw?srn=${encodeURIComponent(f.srn)}`)
           .then(x => x.text())
           .then(q => parse<IHttpOperation>(q))
       )
   );
-
 }
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { json, text, send } from 'micri';
 import micri from 'micri';
 import route from '@stoplight/prism-http/dist/router';
 import mock from '@stoplight/prism-http/dist/mocker';
-import { validateInput } from '@stoplight/prism-http/dist/validator'
+import { validateInput } from '@stoplight/prism-http/dist/validator';
 import { createLogger } from '@stoplight/prism-core/dist/logger';
 import * as typeis from 'type-is';
 import { HttpMethod, IHttpOperation } from '@stoplight/types';
-import grabOperationsSomehow from './grabOperations'
-import { pipe } from 'fp-ts/lib/pipeable'
-import * as R from 'fp-ts/lib/Reader'
-import * as E from 'fp-ts/lib/Either'
-import * as RE from 'fp-ts/lib/ReaderEither'
+import grabOperationsSomehow from './grabOperations';
+import { pipe } from 'fp-ts/lib/pipeable';
+import * as R from 'fp-ts/lib/Reader';
+import * as E from 'fp-ts/lib/Either';
+import * as RE from 'fp-ts/lib/ReaderEither';
 import * as O from 'fp-ts/lib/Option';
 import { ProblemJsonError, IPrismDiagnostic } from '@stoplight/prism-core';
 import { IHttpRequest, IHttpOperationConfig } from '@stoplight/prism-http';
@@ -25,8 +25,8 @@ function createPrismInput(url: URL, body: unknown, method: string) {
     method: method as HttpMethod,
     url: {
       path: url.pathname,
-      query: Object.fromEntries(url.searchParams.entries())
-    }
+      query: Object.fromEntries(url.searchParams.entries()),
+    },
   };
 }
 
@@ -37,37 +37,42 @@ function validateInputAndMock(resource: IHttpOperation, element: IHttpRequest, c
     validateInput({ resource, element }),
     E.fold<IPrismDiagnostic[], unknown, IPrismDiagnostic[]>(validations => validations, () => []),
     validations => mock({ resource, input: { data: element, validations }, config })
-  )
+  );
 }
 
 function readConfigFromQueryString(queryString: URLSearchParams): IHttpOperationConfig {
   return {
     code: queryString.get('__code') || undefined,
     exampleKey: queryString.get('__example') || undefined,
-    dynamic: queryString.get('__dynamic') === 'true'
-  }
+    dynamic: queryString.get('__dynamic') === 'true',
+  };
 }
 
 const server = micri(async function requestHandler(req, res) {
   pipe(
-    O.fromNullable<{ project: string, prismUrl: string[] }>(new URI.Template('/{project}{/prismUrl*}').match(req.url)),
-    O.fold(() => send(res, 404), async params => {
-      const resourcesPromise = grabOperationsSomehow(params.project);
-      const bodyPromise = (typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req));
-      const parsedUrl = new URL(params.prismUrl.join('/'), baseUrl);
-      const configFromQueryString = readConfigFromQueryString(parsedUrl.searchParams);
+    O.fromNullable<{ project: string; serviceName: string; prismUrl: string[] }>(
+      new URI.Template('/{project}/{serviceName}{/prismUrl*}').match(req.url)
+    ),
+    O.fold(
+      () => send(res, 404),
+      async params => {
+        const resourcesPromise = grabOperationsSomehow(params.project, params.serviceName);
+        const bodyPromise = typeis(req, ['application/json', 'application/*+json']) ? json(req) : text(req);
+        const parsedUrl = new URL(params.prismUrl.join('/'), baseUrl);
+        const configFromQueryString = readConfigFromQueryString(parsedUrl.searchParams);
 
-      const body = await bodyPromise;
-      const input = createPrismInput(parsedUrl, body, req.method!);
-      const resources = await resourcesPromise;
+        const body = await bodyPromise;
+        const input = createPrismInput(parsedUrl, body, req.method!);
+        const resources = await resourcesPromise;
 
-      return pipe(
-        RE.fromEither(route({ resources, input })),
-        RE.chain(resource => validateInputAndMock(resource, input, configFromQueryString)),
-        RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
-        RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
-      )(logger);
-    })
+        return pipe(
+          RE.fromEither(route({ resources, input })),
+          RE.chain(resource => validateInputAndMock(resource, input, configFromQueryString)),
+          RE.mapLeft(e => ProblemJsonError.fromPlainError(e)),
+          RE.fold(e => R.of(send(res, e.status, e)), response => R.of(send(res, response.statusCode, response.body)))
+        )(logger);
+      }
+    )
   );
 });
 
